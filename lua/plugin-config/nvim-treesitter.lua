cea220471253096413a347856c68a0b91816ce1b
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-06-22 23:56:17 +0800

    fix: 默认不要折叠{}

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -32,7 +32,4 @@ treesitter.setup({
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- 默认不要折叠
--- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
-vim.opt.foldlevel = 99
-
-
+vim.opt.foldenable = false
