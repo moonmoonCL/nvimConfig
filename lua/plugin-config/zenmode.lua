7327b4cea618e94b36e8ba250c7cc756a92b2538
zhaopeng.cl <zhaopeng.cl@bytedance.com>
2023-06-23 16:32:28 +0800

    feat: zenmode shortcut

--- lua/plugin-config/zenmode.lua
+++ lua/plugin-config/zenmode.lua
@@ -1,8 +1,4 @@
 local api = vim.api
 
-api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
-api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
-api.nvim_set_keymap("n", "<leader>zf", ":TZFocus<CR>", {})
-api.nvim_set_keymap("n", "<leader>zm", ":TZMinimalist<CR>", {})
-api.nvim_set_keymap("n", "<leader>za", ":TZAtaraxis<CR>", {})
+api.nvim_set_keymap("n", "<leader>z", ":TZAtaraxis<CR>", {})
 
